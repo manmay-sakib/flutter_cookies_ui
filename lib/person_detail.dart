d509d6fb9ee273ae9a1ba0b3f96b2acb2a385a21
manmay sakib <monmy.sakib@gmail.com>
2023-07-26 16:29:30 +0600

    Initial commit

---
+++ lib/person_detail.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PersonDetail extends StatelessWidget {
+  const PersonDetail({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Person');
+  }
+}
